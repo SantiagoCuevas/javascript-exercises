dca99b7c8211608f6f3dd54e46dd5d834856df23
Santiago Cuevas <santiagocuevas77@gmail.com>
2023-08-29 14:19:12 -0600

    Complete exercise 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...arg) {
+  for (let i = 0; i < arg.length; i++) {
+    const indexOfArg = array.indexOf(arg[i]);
+    if (indexOfArg === -1) {
+      continue;
+    }
+    array.splice(indexOfArg, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
