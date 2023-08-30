35e3cd7249d62e96a922b51d043c311bddb23ac7
Santiago Cuevas <santiagocuevas77@gmail.com>
2023-08-29 18:19:07 -0600

    Complete exercises 5-7.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (val1, val2) {
+  if (typeof val1 !== "number") {
+    return "ERROR";
+  } else if (typeof val2 !== "number") {
+    return "ERROR";
+  } else if ((val1 || val2) < 0) {
+    return "ERROR";
+  }
 
+  let finalSum = 0;
+  let biggerNum;
+  let smallerNum;
+
+  if (val1 < val2) {
+    biggerNum = val2;
+    smallerNum = val1;
+  } else {
+    biggerNum = val1;
+    smallerNum = val2;
+  }
+
+  for (let i = smallerNum; i <= biggerNum; i++) {
+    finalSum += i;
+  }
+  console.log("Sum", finalSum);
+  return finalSum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrTemp) {
+  const celsiusRounded = ((fahrTemp - 32) * (5 / 9)).toFixed(1);
+  return Number.parseFloat(celsiusRounded);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsTemp) {
+  const fahrenheitRounded = (celsTemp * (9 / 5) + 32).toFixed(1);
+  return Number.parseFloat(fahrenheitRounded);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
