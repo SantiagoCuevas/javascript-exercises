6d57f29a1807b01a16d6b83eb7b70ef0108bbba5
Santiago Cuevas <santiagocuevas77@gmail.com>
2023-09-28 19:02:09 -0600

    Complete exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const reverseStr = str.toLowerCase("").replace(/[^a-z0-9]/g, "");
+  return reverseStr.split("").reverse().join("") === reverseStr;
 };
 
 // Do not edit below this line
