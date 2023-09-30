1f5f598af4b4ce5dc386b48042b6967d07a7cf14
Santiago Cuevas <santiagocuevas77@gmail.com>
2023-09-30 13:25:07 -0600

    Complete exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
 
+  let fibonacciArr = [];
+
+  for (let i = 0; i < num; i++) {
+    i < 2
+      ? fibonacciArr.push(1)
+      : fibonacciArr.push(fibonacciArr[i - 1] + fibonacciArr[i - 2]);
+  }
+  return fibonacciArr[num - 1];
 };
 
 // Do not edit below this line
